e595111e44aab8d19c87d0db2839959f922dbf22
kebin <kebin@huya.com>
2018-12-27 16:50:14 +0800

    1

---
+++ default.js
@@ -0,0 +1,22 @@
+function quickSort(arr) {
+	if (arr.length == 0) {
+		return [];
+	}
+	var left = [];
+	var right = [];
+	var pivot = arr[0];
+	for (var i = 1; i < arr.length; i++) {
+		if (arr[i] < pivot) {
+			left.push(arr[i]);
+		} else {
+			right.push(arr[i]);
+		}
+	}
+	return quickSort(left).concat(pivot, quickSort(right));
+}
+var a = [];
+for (var i = 0; i < 100; ++i) {
+	a[i] = Math.floor((Math.random() * 100) + 1);
+}
+console.log(a)
+console.log(quickSort(a))
